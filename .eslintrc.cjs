a7479d0392ca555d32187b71d635f5c5894b806c
tquang2410 <justicepencil@gmail.com>
2025-07-03 21:10:33 +0700

    Step 8

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'no-unused-vars': true,
-    'no-debugger': 'on',
+    'no-unused-vars': 'off',
+    'no-debugger': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
